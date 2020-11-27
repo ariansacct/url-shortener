6736e191927453b80315e89b3be884b2834fb0ba
Arian Azin <ariansacct@gmail.com>
2020-11-26 22:56:51 -0500

    Add code comments and do some clean-up

--- app.py
+++ app.py
@@ -1,5 +1,4 @@
 import cherrypy
-import pandas as pd
 import random
 import string
 import os
@@ -10,6 +9,10 @@ import configparser
 
 class MyWebService(object):
 
+    '''
+    The constructor creates a two-way dictionary (for memory mode), reads/creates the database (for database mode),
+    establishes a connection to the database, and creates the underlying table if necessary.
+    '''
     def __init__(self):
         self.link_map = bidict()
         self.db_file = 'database.db'
@@ -21,6 +24,11 @@ class MyWebService(object):
         cursor.execute('CREATE TABLE IF NOT EXISTS MAPPINGS (original_link TEXT, random_string TEXT);')
         print('Initialized the program with storage type ' + self.mode + '.')
 
+    '''
+    This function generates a random string of alphabets (of length 6 by default), which means our system as of now can support
+    up to 26 ^ 6 = 308915776 URLs. To increease that, we can either use a bigger character set (e.g., alphanumeric), or
+    increase the length of the random string.
+    '''
     def generate_random_string(self, length = 6):
         letters = string.ascii_lowercase
         print("length: " + str(length))
@@ -28,6 +36,9 @@ class MyWebService(object):
         print("Random String is:", result_str)
         return result_str
 
+    '''
+    This HTML content is loaded at the start of the application.
+    '''
     @cherrypy.expose
     def index(self):
         return """
@@ -47,11 +58,17 @@ class MyWebService(object):
 </body>
 """
 
+    '''
+    This is a helper method that prints the content of the database
+    '''
     def print_database(self):
         cursor = self.conn.cursor()
         cursor.execute('SELECT * FROM MAPPINGS')
         print(cursor.fetchall())
 
+    '''
+    This function stores a mapping from an original link to a random string in the configured storage method
+    '''
     def store_mapping(self, original_link, random_string):
         if self.mode == 'memory':
             self.link_map[original_link] = random_string
@@ -67,6 +84,10 @@ class MyWebService(object):
             print("Insertion into the database completed.")
             self.print_database()
 
+    '''
+    This method is called before a new random string is generated; it checks to see if the URL has already been
+    inserted into the system.
+    '''
     def attempt_to_retrieve(self, original_link):
         if self.mode == 'memory':
             if original_link in self.link_map:
@@ -81,6 +102,11 @@ class MyWebService(object):
                 random_string = fetched_list[0][0]
             return random_string
 
+    '''
+    This method checks if a generated random string has already been assigned to a URL. Chances of this happening is
+    very slim (also depends on how many random strings have already been used), but to ensure correctness this check
+    has to be performed.
+    '''
     def is_duplicate(self, random_string):
         if self.mode == 'memory':
             if random_string in self.link_map.values():
@@ -95,6 +121,11 @@ class MyWebService(object):
             return False
 
 
+    '''
+    This method handles a shortening request for a URL. It first checks to see if that URL has already been inserted
+    into the system. If not, generates a random string and checks for its uniqueness. It then stores the mapping in the
+    specified storage type.
+    '''
     @cherrypy.expose
     def shorten(self, original_link):
         new_entry = False
@@ -119,6 +150,10 @@ class MyWebService(object):
                     </body>
                     """.format(shortened_link = shortened_link)
 
+    '''
+    This method handles a retrieval request for a URL. It first extracts the random string from the shortened URL. The
+    corresponding URL is then retrieved from the specified storage type. If the URL is not found an error message is shown.
+    '''
     @cherrypy.expose
     def retrieve(self, shortened_link):
         random_string = shortened_link.split('/')[1]
@@ -141,9 +176,6 @@ class MyWebService(object):
 
 
 if __name__ == '__main__':
-    config = {'server.socket_host': '0.0.0.0'}
-    cherrypy.config.update(config)
-
     conf = {
         '/': {
             'tools.sessions.on': True,
@@ -155,8 +187,6 @@ if __name__ == '__main__':
         }
     }
     my_web_service = MyWebService()
-    my_web_service.print_database()
+    # to print the existing database contents before application start-up
+    # my_web_service.print_database()
     cherrypy.quickstart(my_web_service, '/', conf)
-
-    # cherrypy.quickstart(MyWebService(), '/', config)
-    # cherrypy.quickstart(MyWebService())
